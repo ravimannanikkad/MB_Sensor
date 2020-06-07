94186d7c6f51a6c572aae2dd62c5a142ecdd601f
Ravi Damodaran <ravimannanikkad@gmail.com>
2020-06-07 11:54:10 +0200

    First commit

---
+++ MB_Sensor/Applications/Debug/src/IMU_raw.d
@@ -0,0 +1,55 @@
+src/IMU_raw.o src/IMU_raw.o: ../src/IMU_raw.c \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xiic.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_types.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_assert.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xstatus.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xiic_l.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_io.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/bspconfig.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/mb_interface.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_exception.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xintc.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xintc_l.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_exception.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/sleep.h \
+ C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_printf.h \
+ ../src/i2cCustom.h
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xiic.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_assert.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xstatus.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xiic_l.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_io.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/bspconfig.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/mb_interface.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_exception.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xintc.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xintc_l.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_exception.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/sleep.h:
+
+C:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include/xil_printf.h:
+
+../src/i2cCustom.h:
---
+++ MB_Sensor/Applications/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/IMU_raw.c \
+../src/i2cCustom.c 
+
+OBJS += \
+./src/IMU_raw.o \
+./src/i2cCustom.o 
+
+C_DEPS += \
+./src/IMU_raw.d \
+./src/i2cCustom.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze gcc compiler'
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -IC:/Users/ravim/OneDrive/Desktop/Vitis/MB_Sensor/export/MB_Sensor/sw/MB_Sensor/standalone_domain/bspinclude/include -mlittle-endian -mxl-barrel-shift -mxl-pattern-compare -mno-xl-soft-div -mcpu=v11.0 -mno-xl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
